7d5d8995b2e4ed2d37f0fea7064a6dff8989460a
dewagedecahyaputra <dewagedecahyaputra07@gmail.com>
2025-04-29 22:33:33 +0800

    SuratBerhargaNegara Class

---
+++ src/SuratBerhargaNegara.java
@@ -0,0 +1,15 @@
+public class SuratBerhargaNegara {
+    String nama;
+    double bunga;
+    int jangkaWaktu;
+    String tanggalJatuhTempo;
+    double kuotaNasional;
+
+    public SuratBerhargaNegara(String nama, double bunga, int jangkaWaktu, String tanggalJatuhTempo, double kuotaNasional) {
+        this.nama = nama;
+        this.bunga = bunga;
+        this.jangkaWaktu = jangkaWaktu;
+        this.tanggalJatuhTempo = tanggalJatuhTempo;
+        this.kuotaNasional = kuotaNasional;
+    }
+}

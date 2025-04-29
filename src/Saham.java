052a9dde3c109392bd28dbce21d7f19d4b7af131
dewagedecahyaputra <dewagedecahyaputra07@gmail.com>
2025-04-29 22:31:30 +0800

    Saham Class

---
+++ src/Saham.java
@@ -0,0 +1,10 @@
+public class Saham {
+    String kode, namaPerusahaan;
+    double harga;
+
+    public Saham(String kode, String namaPerusahaan, double harga) {
+        this.kode = kode;
+        this.namaPerusahaan = namaPerusahaan;
+        this.harga = harga;
+    }
+}

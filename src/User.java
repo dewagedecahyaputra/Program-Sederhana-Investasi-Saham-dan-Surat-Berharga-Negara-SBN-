4a1666473a6aebd9a73e3276e8bc1057e07425f0
dewagedecahyaputra <dewagedecahyaputra07@gmail.com>
2025-04-29 22:29:38 +0800

    Class User

---
+++ src/User.java
@@ -0,0 +1,9 @@
+public class User {
+    String username, password, role;
+
+    public User(String username, String password, String role) {
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
+}
